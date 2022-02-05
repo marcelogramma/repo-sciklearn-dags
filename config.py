4648bece1a56fe38a00e42737ea543d7b4c084b2
marcelogramma <marcelogrammatico02@gmail.com>
2022-02-05 20:58:52 -0300

    Fix config.py

--- config.py
+++ config.py
@@ -15,15 +15,5 @@ engine2 = create_engine(
     f"postgresql://{DB_USER}:{DB_PASS}@{IP}:{DB_PORT}/{DB_NAME_BASE}"
 )
 
-conn_dic = {
-    "DB_USER": "postgres",
-    "DB_PASS": "postgres",
-    "IP": "ml-rds-postgres.cxer4qyvikrv.us-east-1.rds.amazonaws.com",
-    "DB_PORT": "5432",
-    "DB_NAME": "ml-rds-postgres-from-s3",
-    "DB_NAME_BASE": "postgres",
-    "TBL_NAME" = "ml_table_from_s3",
-}
-
 if __name__ == "__main__":
     ()

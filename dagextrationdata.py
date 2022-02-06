9fb13ac2531577a0782814e48df34d685f4e4648
marcelogramma <marcelogrammatico02@gmail.com>
2022-02-05 22:02:20 -0300

    Fix Dag Extration data

--- dagextrationdata.py
+++ dagextrationdata.py
@@ -61,6 +61,7 @@ with DAG(
     "extract_froms3_to_postgres",
     default_args=DAG_DEFAULT_ARGS,
     schedule_interval="0 3 * * *",
+    start_date=days_ago(1),
     catchup = False) as dag:
 
     from_s3 = S3KeySensor(task_id = 'from_s3_task',

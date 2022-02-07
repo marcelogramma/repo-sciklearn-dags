0c69e509e2e501747f7f9c88e70e4aded387de86
marcelogramma <marcelogrammatico02@gmail.com>
2022-02-07 20:27:44 -0300

    Updated config.py

--- dagextrationdata.py
+++ dagextrationdata.py
@@ -40,7 +40,7 @@ def extract_load_data():
     )
     create_table.close()
 
-    insert = """COPY {config.TBL_NAME}(fl_date, op_carrier, op_carrier_fl_num, origin, dest, crs_dep_time, dep_time,\
+    insert = """COPY ml_table_from_s3(fl_date, op_carrier, op_carrier_fl_num, origin, dest, crs_dep_time, dep_time,\
             dep_delay, taxi_out, wheels_off, wheels_on, taxi_in, crs_air_time, arr_time, arr_delay, cancelled, cancellation_code,\
                 diverted, crs_elapsed_time, actual_elapsed_time, air_time, distance, carrier_delay, wheater_delay, nas_delay,\
                     security_delay, late_aircraft_delay, unnamed)

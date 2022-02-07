c68c7db5006589a96f999c27be2d08881839cad5
marcelogramma <marcelogrammatico02@gmail.com>
2022-02-07 20:59:15 -0300

    Updated config.py

--- dagextrationdata.py
+++ dagextrationdata.py
@@ -40,15 +40,16 @@ def extract_load_data():
     )
     create_table.close()
 
-    insert = """COPY ml_table_from_s3(fl_date, op_carrier, op_carrier_fl_num, origin, dest, crs_dep_time, dep_time,
+    insert = con.execute(
+        """
+        INSERT INTO ml_table_from_s3(fl_date, op_carrier, op_carrier_fl_num, origin, dest, crs_dep_time, dep_time,
             dep_delay, taxi_out, wheels_off, wheels_on, taxi_in, crs_air_time, arr_time, arr_delay, cancelled, cancellation_code,
                 diverted, crs_elapsed_time, actual_elapsed_time, air_time, distance, carrier_delay, wheater_delay, nas_delay,
                     security_delay, late_aircraft_delay, unnamed)
-        FROM '(raw_df)'
-        DELIMITER ',' 
-        CSV HEADER;"""
-    con.execute(insert)
-    con.close()
+        VALUES '(raw_df)'
+        """
+    )
+    insert.close()
     
     print(f"Data inserted into {config.DB_NAME}...")
 

1ab09626f31a58d208babae95d52d7b8c7d6d505
marcelogramma <marcelogrammatico02@gmail.com>
2022-02-11 10:52:46 -0300

    Update dagextrationdata.py

--- dagextrationdata.py
+++ dagextrationdata.py
@@ -6,6 +6,7 @@ from airflow.models import DAG
 from airflow.utils.dates import days_ago
 import awswrangler as wr
 from airflow.operators.python_operator import PythonOperator
+import s3fs
 
 ######################################################################
 #
@@ -43,15 +44,18 @@ def extract_load_data2009():
     print(f"Data written to {config.DB_NAME}")
 
 ######################################################################
-#              Load data to Postgres 2009
-#               insersion en la S3 como csv
+#              Load data to Postgres 2009 &
+#               guardado en S3 como csv
 ######################################################################
 
-    raw_ave_delay2009.to_csv(
-        f"s3://{config.BUCKET_RAW}/raw-out/ave_delay_2009.csv",
+    wr.s3.to_csv(
+        df=pd.DataFrame(raw_ave_delay2009),
+        path=f"s3://{config.BUCKET_RAW}/raw_out/2009.csv",
+        sep=",", 
         index=True,
+        header=True,
     )
-    print(f"Data written to {config.BUCKET_RAW}/raw-out/ave_delay_2009.csv")
+    print(f"Data written to {config.BUCKET_RAW}/raw-out/2009.csv")
 
 
 ######################################################################
